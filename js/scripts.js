285c7c4aa62405dbb6eed36f9043c59bce7ca9b6
Andres Acuña <andres.cbaa@gmail.com>
2021-02-25 08:41:58 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+(function($){
+    var typed = new Typed ('span.txt-rotate',{
+   	strings: ["SOPORTEC!"],
+   	typeSpeed: 100, 
+   	backSpeed: 100,
+   	fadeOut: false,
+   	smartBackspace: true,
+   	loop: true
+   });
+})(jQuery);
